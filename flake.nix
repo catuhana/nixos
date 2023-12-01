bf3a79ed650949aaadd7c405967ac38fce8ccdee
tuhana <github@tuhana.me>
2023-12-01 12:29:22 +0300

    agenix

--- flake.nix
+++ flake.nix
@@ -2,16 +2,28 @@
   description =
     "Unified configuration for my server and probably my desktops too.";
 
-  inputs = { nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable"; };
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    agenix.url = "github:ryantm/agenix";
+  };
 
-  outputs = { self, nixpkgs, ... }@inputs: {
+  outputs = { self, nixpkgs, agenix, ... }@inputs: {
     nixosConfigurations = {
       "meow" = nixpkgs.lib.nixosSystem {
         system = "aarch64-linux";
 
         specialArgs = { inherit inputs; };
 
-        modules = [ ./meow/default.nix ./config.nix ];
+        modules = [
+          ./meow/default.nix
+          ./config.nix
+          agenix.nixosModules.default
+          {
+            environment.systemPackages = [
+              agenix.packages.${system}.default
+            ]
+          }
+        ];
       };
     };
   };
