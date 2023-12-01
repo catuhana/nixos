e2f5ba7d5f3d53a8885de85c8da9757bfc4adc50
tuhana <github@tuhana.me>
2023-12-01 11:03:12 +0300

    me dumb

--- meow/self-hosted/postgresql.nix
+++ meow/self-hosted/postgresql.nix
@@ -3,7 +3,7 @@
         enable = true;
         enableJIT = true;
 
-        services.postgresql.authentication = lib.mkForce ''
+        authentication = lib.mkForce ''
             local   all             all                                     trust
             host    all             all             127.0.0.1/32            trust
             host    all             all             ::1/128                 trust
