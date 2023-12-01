6ff28b7d8945893f89b8b93ba84c6f9416c514ab
tuhana <github@tuhana.me>
2023-12-01 10:57:46 +0300

    famf

--- meow/self-hosted/postgresql.nix
+++ meow/self-hosted/postgresql.nix
@@ -1,7 +1,11 @@
 { ... }: {
     services."postgresql" = {
         enable = true;
+        enableJIT = true;
 
+        ensureUsers = [
+            { name = "forgejo"; ensureDBOwnership = true; }
+        ];
         ensureDatabases = [ "forgejo" ];
     };
 }
