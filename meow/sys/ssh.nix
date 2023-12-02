c7c5a0cdce5c9dd1330fb6e92efd739d45dad31f
tuhana <git@tuhana.me>
2023-12-02 23:19:38 +0100

    funsies

--- meow/sys/ssh.nix
+++ meow/sys/ssh.nix
@@ -2,6 +2,23 @@
   services.openssh = {
     enable = true;
 
+    banner = ''
+                                                __     
+                                              /'__`\   
+  ___ ___      __    ___   __  __  __      __/\_\L\ \  
+/' __` __`\  /'__`\ / __`\/\ \/\ \/\ \    /\_\/_/_\_<_ 
+/\ \/\ \/\ \/\  __//\ \L\ \ \ \_/ \_/ \   \/_/_/\ \L\ \
+\ \_\ \_\ \_\ \____\ \____/\ \___x___/'     /\_\ \____/
+ \/_/\/_/\/_/\/____/\/___/  \/__//__/       \/_/\/___/ 
+
+
+    '';
+
+    extraConfig = ''
+      ClientAliveInterval 600
+      ClientAliveCountMax 3
+    '';
+
     settings = {
       PermitRootLogin = "no";
       PasswordAuthentication = false;
