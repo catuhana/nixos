7f66552e95b4391451d675e27eabb95df6d82fc4
tuhana <git@tuhana.me>
2024-01-12 18:23:06 +0100

    update lockfile and add jvmopts

--- meow/services/self-hosted/minecraft/default.nix
+++ meow/services/self-hosted/minecraft/default.nix
@@ -5,6 +5,8 @@
     eula = true;
     declarative = true;
 
+    jvmOpts = "-XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M";
+
     serverProperties = {
       level-name = "242";
       online-mode = false;
