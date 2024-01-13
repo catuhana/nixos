1c40762ab9c26f41d90d029973ee09c42205cf68
tuhana <git@tuhana.me>
2024-01-13 23:14:55 +0100

    adjust mc server

--- meow/services/self-hosted/minecraft/default.nix
+++ meow/services/self-hosted/minecraft/default.nix
@@ -5,18 +5,19 @@
     eula = true;
     declarative = true;
 
-    jvmOpts = "-XX:+UseG1GC -Xmx4G -Xms4G -Dsun.rmi.dgc.server.gcInterval=2147483646 -XX:+UnlockExperimentalVMOptions -XX:G1NewSizePercent=20 -XX:G1ReservePercent=20 -XX:MaxGCPauseMillis=50 -XX:G1HeapRegionSize=32M";
+    jvmOpts = "-Xms4096M -Xmx4096M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20";
 
     serverProperties = {
       level-name = "242";
       online-mode = false;
       max-players = 5;
 
+      sync-chunk-writes = false;
       view-distance = 12;
       simulation-distance = 12;
 
       gamemode = "survival";
-      difficulty = "hard";
+      difficulty = "normal";
 
       motd = "Oda 242";
     };
