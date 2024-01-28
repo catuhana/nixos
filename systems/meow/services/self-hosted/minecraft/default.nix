f26fa2d7043a33041976d2a07b4fde45b95743a2
tuhana <git@tuhana.me>
2024-01-28 21:21:31 +0100

    remove dumb shit and add krypton plugin for mc

--- systems/meow/services/self-hosted/minecraft/default.nix
+++ systems/meow/services/self-hosted/minecraft/default.nix
@@ -21,7 +21,7 @@ in
 
       package = pkgs.fabricServers.fabric-1_20_4;
 
-      jvmOpts = "-Xms4096M -Xmx4096M -XX:+UseG1GC -XX:+ParallelRefProcEnabled -XX:MaxGCPauseMillis=200 -XX:+UnlockExperimentalVMOptions -XX:+DisableExplicitGC -XX:+AlwaysPreTouch -XX:G1HeapWastePercent=5 -XX:G1MixedGCCountTarget=4 -XX:InitiatingHeapOccupancyPercent=15 -XX:G1MixedGCLiveThresholdPercent=90 -XX:G1RSetUpdatingPauseTimePercent=5 -XX:SurvivorRatio=32 -XX:+PerfDisableSharedMem -XX:MaxTenuringThreshold=1 -Dusing.aikars.flags=https://mcflags.emc.gs -Daikars.new.flags=true -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20 -XX:G1NewSizePercent=30 -XX:G1MaxNewSizePercent=40 -XX:G1HeapRegionSize=8M -XX:G1ReservePercent=20";
+      jvmOpts = "-Xms4G -Xmx4G -XX:+UseG1GC";
 
       whitelist = {
         catuhana = "46854c64-da1e-3955-bf52-6b9eca8aec41";
@@ -70,6 +70,11 @@ in
           FerriteCore = fetchurl {
             url = "https://cdn.modrinth.com/data/uXXizFIs/versions/pguEMpy9/ferritecore-6.0.3-fabric.jar";
             hash = "sha256-ZphO6VxKsFqDxDew3QRWikosjn8hm3SYYE4llSxMwSU=";
+          };
+
+          Krypton = fetchurl {
+            url = "https://cdn.modrinth.com/data/fQEb0iXm/versions/bRcuOnao/krypton-0.2.6.jar";
+            hash = "sha256-cgJMTT+ojERKtDoG917YQDsgExxHJfT/Zm34scpR6xU=";
           };
 
           MemoryLeakFix = fetchurl
